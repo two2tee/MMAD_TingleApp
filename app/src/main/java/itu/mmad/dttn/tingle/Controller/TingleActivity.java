951177716006d27f9a46a9d3de20fbc80123bd5a
Dennis Thinh Tan Nguyen <dttn@itu.dk>
2016-02-17 21:43:13 +0100

    Merged JavaDocBranch to developer branch

--- app/src/main/java/itu/mmad/dttn/tingle/Controller/TingleActivity.java
+++ app/src/main/java/itu/mmad/dttn/tingle/Controller/TingleActivity.java
@@ -3,7 +3,6 @@ package itu.mmad.dttn.tingle.Controller;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
 import itu.mmad.dttn.tingle.Controller.Fragments.ListFragment;
 import itu.mmad.dttn.tingle.Controller.Fragments.TingleFragment;
